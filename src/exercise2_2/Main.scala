c16f5dc9a89eb98b24868c949a9dbece4342f9c6
T45K <tasktas9@gmail.com>
2022-06-03 21:52:37 +0900

    add plumorphic function

---
+++ src/exercise2_2/Main.scala
@@ -0,0 +1,23 @@
+package exercise2_2
+
+import scala.annotation.tailrec
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println(Array(7, 9, 13).isSorted((x: Int, y: Int) => x <= y))
+  }
+
+  implicit class ExtendArray[A](as: Array[A]) {
+    def isSorted(ordered: (A, A) => Boolean): Boolean = {
+      @tailrec
+      def loop(n: Int): Boolean = {
+        if (n == as.length) true
+        else if (ordered(as(n - 1), as(n))) loop(n + 1)
+        else false
+      }
+
+      loop(1)
+    }
+  }
+}
