3e95cf548fc0fdbe06c4c5887642b650c2cef02b
T45K <tasktas9@gmail.com>
2022-06-13 22:49:21 +0900

    update

--- src/exercise6/State.scala
+++ src/exercise6/State.scala
@@ -115,4 +115,13 @@ object State {
   def sequence[S, A](ss: List[State[S, A]]): State[S, List[A]] = ss.foldRight(unit(Nil): State[S, List[A]]) { (s, acc) =>
     s.map2(acc)((h, t) => h :: t)
   }
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => () -> s)
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
